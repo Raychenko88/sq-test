dcf77cef44acd7dbc50afc137b76e1cdeb1291a1
Sergey <sergeyraychenko@gmail.com>
2021-07-25 17:44:45 +0300

    8 spin commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "9 spin commit" );
+        System.out.println( "8 spin commit" );
     }
 }
