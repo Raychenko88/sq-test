38ed880e7f33910fd248b449214f6cb51b6c4b59
Sergey <sergeyraychenko@gmail.com>
2021-07-25 16:29:06 +0300

    3 spin commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "2 spin commit" );
+        System.out.println( "3 spin commit" );
     }
 }
