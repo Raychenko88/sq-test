1c8c340ff3786027a13859e40ce9b167d8e56f39
Sergey <sergeyraychenko@gmail.com>
2021-09-12 20:53:10 +0300

    9 method

--- src/main/java/org/example/TestClass2.java
+++ src/main/java/org/example/TestClass2.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class TestClass2 {
     public void test2() {
-        System.out.println("8 test spin method");
+        System.out.println("9 test spin method");
     }
 }
