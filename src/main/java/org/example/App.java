aa9b4ddcf842100f1a1b5bc4d29f785acce17f0b
Sergey <sergeyraychenko@gmail.com>
2021-09-12 18:18:51 +0300

    12

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "11 fix commit" );
+        System.out.println( "12 fix commit" );
     }
 }
