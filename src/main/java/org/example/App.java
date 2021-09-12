04ef69263d2d4f6e5624a515ab74e8bc61a45975
Sergey <sergeyraychenko@gmail.com>
2021-09-12 12:36:25 +0300

    FPB-0: first develop commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "1 develop commit" );
 //        System.out.println( "1 develop commit" );
+        System.out.println( "FIRST develop commit" );
     }
 }
